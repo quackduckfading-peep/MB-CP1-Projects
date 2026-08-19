ddbcc2cf38610fd86993b2bfcd269d876720f57e
quackduckfading-peep <mariam.brown@ucas-edu.net>
2026-08-19 09:22:38 -0600

    This is my Hello World Project

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+# Ren Brown, Hello World
+
+print("Hello world!")
+
+name = input("What is your name?")
+
+print("Hello" + name)
+
+print("It is nice to meet you," + name)
+
+print("I am your computer. You can call me Peter," + name)
