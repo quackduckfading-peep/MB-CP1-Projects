af4df98fea397ef3616b33aec011c33b6353bfe4
quackduckfading-peep <mariam.brown@ucas-edu.net>
2026-08-20 09:54:29 -0600

    made character intro

---
+++ character_intro.py
@@ -0,0 +1,21 @@
+# Ren Brown, Character Intro
+
+name = input("Hello. My name is Reno. What is your name?")
+
+print("That is a nice name," + name)
+
+age = input("I am 14 years old. What is your age?")
+
+print(age, " is a cool age to be!")
+
+job = input("My job is drawing. I'm an artist. I also write stories. What's your job?")
+
+input("Being a/an" + job + " must be great! Do you like your job?")
+
+print("That's good. I'm glad you like it.")
+
+location = input("I'm from Colorado. Where are you from?")
+
+input("I don't think I've been there yet! I hear that" + location + " is a gorgeous place. Is that true?")
+
+print("That's awesome!!")
