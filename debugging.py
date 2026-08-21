74c2e7c961acd580845d1ead9abf75654a3ed5a7
quackduckfading-peep <mariam.brown@ucas-edu.net>
2026-08-21 09:19:22 -0600

    made a new file for debugging

---
+++ debugging.py
@@ -0,0 +1,8 @@
+# Ren Brown, Debugging
+
+name = "Alex"
+age = 16
+
+print("My name is " + name + " and I am " + str(age) + " years old." )
+# Error 1: name was spelled wrong
+# Error 2: ther was a missing parentheses
