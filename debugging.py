23f60b93b5f378843100d359c8d15f2ac78afc10
quackduckfading-peep <mariam.brown@ucas-edu.net>
2026-08-24 08:58:33 -0600

    fixed spelling for a comment

--- debugging.py
+++ debugging.py
@@ -5,4 +5,4 @@ age = 16
 
 print("My name is " + name + " and I am " + str(age) + " years old." )
 # Error 1: name was spelled wrong
-# Error 2: ther was a missing parentheses
+# Error 2: there was a missing parentheses
