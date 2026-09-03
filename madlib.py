664601881bbeddef3811d0e119cdcfcd9b766895
quackduckfading-peep <mariam.brown@ucas-edu.net>
2026-09-03 09:53:30 -0600

    created a madlib from project sekai

---
+++ madlib.py
@@ -0,0 +1,14 @@
+# Ren Brown, Madlib
+
+adjective = input("Give me an adjective: ")
+pjsk_name = input("Give me a character from pjsk: ")
+noun = input("Give me a noun: ")
+food = input("Give me a food or dish: ")
+number = input("Give me a number: ")
+feeling = input("Give me an emotion or feeling: ")
+action = input("Give me an action or verb that is past tense: ")
+place = input("Give me a place in Project Sekai: ")
+sound = input("Give me a silly sound or an exclamation: ")
+
+
+print("Today, Miku woke up feeling extra " + adjective + ", ready to got to " + place + ". Suddenly, " + pjsk_name + " jumped out of a bush and yelled " + sound + "'! We need your help! SEKAI is breaking!' Miku crosses her arms and asked that she be givin " + food + " or she would not sing for " + number + " whole days. Feeling very " + feeling + ", " + pjsk_name + " sighed and" + action + " away.")
