d37ab2c804bb0b66772d5a7b21599d160ce3ebb3
quackduckfading-peep <mariam.brown@ucas-edu.net>
2026-09-03 09:04:19 -0600

    trieed to fix it

---
+++ average_grade.py
@@ -0,0 +1,9 @@
+# Ren Brown, Average Grade
+
+first_grade = input("What is your grade in first period? ")
+second_grade = input("What is your grade in second period? ")
+third_grade = input("What is your grade in third period? ")
+fourth_grade = input("What is your grade in fourth period? ")
+fifth_grade = input("What is your grade in fifth period? ")
+sixth_grade = input("What is your grade in sixth period? ")
+seventh_grade = input("What is your grade in seventh period?")
