66453dd09310b2c82e972437c4c86230ccff1832
quackduckfading-peep <mariam.brown@ucas-edu.net>
2026-09-08 09:20:17 -0600

    added more notes

--- p1_notes_string_methonds.py
+++ p1_notes_string_methonds.py
@@ -24,3 +24,13 @@ print(full_name.isnumeric()) # Checks if the entire thing is numbers
 print(full_name.isupper()) # Checks is all the string is uppercase
 
 print(f"Hello {full_name} welcome to my program.") # lets us not throw in extra things like spaces or +'s
+
+# formatted string
+print(f"hello {fixed.title()} {last_fixed} welcome to my program.") """
+
+letter = input("Give me a letter: ")
+letter = letter[0].lower()
+number_value = ord(letter)
+number_value += 2
+new_letter +chr(number_value)
+print(f"your letter was {letter} now it is {new_letter}") # Does stuff
