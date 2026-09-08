fef95d38830386ec6923b619c63606efdccaa1a5
quackduckfading-peep <mariam.brown@ucas-edu.net>
2026-09-08 08:53:15 -0600

    i added an '

--- madlib.py
+++ madlib.py
@@ -8,7 +8,7 @@ number = input("Give me a number: ")
 feeling = input("Give me an emotion or feeling: ")
 action = input("Give me an action or verb that is past tense: ")
 place = input("Give me a place in Project Sekai: ")
-sound = input("Give me a silly sound or an exclamation: ")
+sound = input("Give me a silly sound or an exclamation: '")
 
 
 print("Today, Miku woke up feeling extra " + adjective + ", ready to got to " + place + ". Suddenly, " + pjsk_name + " jumped out of a bush and yelled " + sound + "'! We need your help! SEKAI is breaking!' Miku crosses her arms and asked that she be givin " + food + " or she would not sing for " + number + " whole days. Feeling very " + feeling + ", " + pjsk_name + " sighed and" + action + " away.")
