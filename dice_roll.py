646b7dbad082a37de49ad9de0bc790a51754ee00
quackduckfading-peep <mariam.brown@ucas-edu.net>
2026-09-09 09:52:02 -0600

    made notes and created an assignment thing

---
+++ dice_roll.py
@@ -0,0 +1,2 @@
+# Ren Brown, Dice Roller
+
---
+++ using_random.py
@@ -0,0 +1,14 @@
+import random
+
+ducks = random.randint(1,10) # Takes ONLY 2 arguements in parenthesis to properoy pull a random thing
+
+print(f"There are {ducks} ducks!") # We are so creative with out code guys, look.
+
+pens = random.randrange(2,100,2) # The last two is what it's counting by
+print(f"I have {pens} pens.") # Still oh so creative.
+
+percent = random.random()
+
+print(f"You have a {percent:.2} grade.") # The :.2 is just for visual purposes it doesn't change the actual long decimal.
+
+# DON'T TRUST FLOATS. THEY'RE UNTRUSTWORTHY. Just like lightweavers.
