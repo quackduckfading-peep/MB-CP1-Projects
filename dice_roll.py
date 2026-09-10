184bc2e8b26372f3ef9d450de3b0075122e1f051
quackduckfading-peep <mariam.brown@ucas-edu.net>
2026-09-10 09:53:08 -0600

    coded

--- dice_roll.py
+++ dice_roll.py
@@ -1,2 +1,29 @@
 # Ren Brown, Dice Roller
 
+import random
+
+while True:
+    try:
+        dice = input(f"I have: D4, D6, D6, D8, D10, D12, and a D20 dice. Choose one please: D")
+    except:
+        print("That was not an option I gave you. Try again.")
+    else:
+        break
+
+
+
+d4 = random.randint(1,4)
+d6 = random.randint(1,6)
+d8 = random.randint(1,8)
+d10 = random.randint(1,10)
+d12 = random.randint(1,12)
+d20 = random.randint(1,20)
+
+if dice == 4:
+    print(f"You rolled a {d4}.")
+if dice == 6:
+    print(f"You rolled a {d6}.")
+if dice == 8:
+    print(f"You rolled a {d8}.")
+if dice == 10:
+    print(f"You rolled a {d10}.")
--- using_random.py
+++ using_random.py
@@ -1,6 +1,6 @@
 import random
 
-ducks = random.randint(1,10) # Takes ONLY 2 arguements in parenthesis to properoy pull a random thing
+ducks = random.randint(1,10) # Takes ONLY 2 arguements in parenthesis to properly pull a random thing
 
 print(f"There are {ducks} ducks!") # We are so creative with out code guys, look.
 
@@ -12,3 +12,5 @@ percent = random.random()
 print(f"You have a {percent:.2} grade.") # The :.2 is just for visual purposes it doesn't change the actual long decimal.
 
 # DON'T TRUST FLOATS. THEY'RE UNTRUSTWORTHY. Just like lightweavers.
+
+# The answer is cancer.
