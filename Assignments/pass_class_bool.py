043f1946c2ce75acfbe66ce652d619b510c99ee7
quackduckfading-peep <mariam.brown@ucas-edu.net>
2026-09-24 09:03:46 -0600

    made a file tp check if you are passing

---
+++ Assignments/pass_class_bool.py
@@ -0,0 +1,9 @@
+# Ren Brown, Boolean Statement
+
+grade = int(input("What is your grade? "))
+
+if grade > 60:
+    print("You are passing the class.")
+
+if grade < 60:
+    print("You are not passing.")
