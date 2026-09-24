2518d4d141873f4eb04f2344d341abed1905ca7c
quackduckfading-peep <mariam.brown@ucas-edu.net>
2026-09-24 09:52:59 -0600

    made notes and started assignment

---
+++ Assignments/user_sign_in.py
@@ -0,0 +1,7 @@
+# Ren Brown, 1, user Sign In
+
+username = input("What is your username: ")
+password = input("What is your password: ")
+
+if username == "Larose":
+    input("What is your password: ")
---
+++ Notes/conditionals_notes.py
@@ -0,0 +1,18 @@
+# RB, Conditional Notes
+grade = 74
+
+if grade >= 90:
+    print("You have an A! Good Job!")
+elif grade >= 70:
+    print("You are passing!")
+else:
+    print("You are not passing.")
+    print("Do you need to retake a quiz? Or do you need to submit a missing assignment?")
+
+
+raining = False
+
+if raining:
+    print("Bring an umbrella.")
+else:
+    print("Wear sunscreen.")
