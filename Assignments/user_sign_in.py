1363d66976af6a72339d6ff4ffc2f54f414acbe6
quackduckfading-peep <mariam.brown@ucas-edu.net>
2026-09-25 09:20:22 -0600

    let it sign in using a student account and a teacher account

--- Assignments/user_sign_in.py
+++ Assignments/user_sign_in.py
@@ -3,5 +3,23 @@
 username = input("What is your username: ")
 password = input("What is your password: ")
 
-if username == "Larose":
-    input("What is your password: ")
+if username == "LaRose":
+    if password == "TeAtChEr":
+        print("Congratulations, you are signed in!")
+        print("Moving onto the next step.")
+    else:
+        print("Username or Password are incorrect.")
+        print("Is this even the right account for you?")
+elif username == "Student":
+   #print("Moving onto the next step.")
+
+    if password == "sTuDeNt":
+        print("Congratulations, you are signed in!")
+#else:
+  # print("That is not a valid account. Please try again.")
+    else:
+        print("Username or Passwoed are incorrect.")
+        print("Is this even the right account for you?")
+
+else:
+   print("That is not a valid account. Please try again.")
